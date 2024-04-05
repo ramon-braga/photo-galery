9478c66a06e2577aa91cc2a8a742f7b1024fa9c9
Ramon Braga <140903176+ramon-braga@users.noreply.github.com>
2024-04-05 13:05:35 -0300

    created data and types for photos

---
+++ src/data/photoList.ts
@@ -0,0 +1,13 @@
+import { Photo } from "@/types/Photo";
+
+export const photoList: Photo[] = [
+    { id: 1, url: '/photos/1.jpg' },
+    { id: 2, url: '/photos/2.jpg' },
+    { id: 3, url: '/photos/3.jpg' },
+    { id: 4, url: '/photos/4.jpg' },
+    { id: 5, url: '/photos/5.jpg' },
+    { id: 6, url: '/photos/6.jpg' },
+    { id: 7, url: '/photos/7.jpg' },
+    { id: 8, url: '/photos/8.jpg' },
+    { id: 9, url: '/photos/9.jpg' }
+];
