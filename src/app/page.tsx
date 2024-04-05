0d755f70a904c4b3326e97124873e1aee393af5a
Ramon Braga <140903176+ramon-braga@users.noreply.github.com>
2024-04-05 13:12:15 -0300

    prepared page ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import { photoList } from "@/data/photoList";
+
 function Page() {
   return (
-    <div>
-      ...
+    <div className="mx-6">
+      <h1 className="text-center my-9 text-4xl">Photo Galery</h1>
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {photoList.map(item =>
+          <div>
+            ...
+          </div>
+        )}
+      </div>
     </div>
   );
 }
